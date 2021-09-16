799ccfab2df64dd4ffbde46d0ab01f010aa257bd
guilsa <guilsa001@gmail.com>
2021-09-16 14:38:56 -0700

    Simplify Textarea should auto expand height

--- app/journals/components/Textarea.tsx
+++ app/journals/components/Textarea.tsx
@@ -1,21 +1,15 @@
 import { useRef, useState, useEffect, SyntheticEvent, ChangeEvent } from "react"
 
 export const Textarea = ({ initialData, handleChange, ...props }) => {
-  const [value, setValue] = useState<String>()
   const textareaRef = useRef<HTMLTextAreaElement | null>(null)
-
-  const handleChangeLocal = (e: React.FormEvent<HTMLInputElement>) => {
-    setValue(e.currentTarget.value)
-    handleChange(e.currentTarget.value)
-  }
 
   useEffect(() => {
     if (textareaRef && textareaRef.current) {
-      textareaRef.current.style.height = "0px"
+      textareaRef.current.style.height = "600px"
       const scrollHeight = textareaRef.current.scrollHeight
       textareaRef.current.style.height = scrollHeight + "px"
     }
-  }, [value])
+  }, [initialData])
 
   return (
     <>
@@ -24,14 +18,13 @@ export const Textarea = ({ initialData, handleChange, ...props }) => {
         style={{
           fontSize: 16,
           width: "100%",
-          height: "600px",
           border: "none",
           outline: 0,
           resize: "none",
           overflow: "hidden",
         }}
         value={initialData}
-        onChange={(e) => handleChangeLocal(e)}
+        onChange={(e) => handleChange(e.currentTarget.value)}
         // {...props}
       ></textarea>
     </>
